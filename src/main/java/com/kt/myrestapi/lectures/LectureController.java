dc15cac6897187436346a50eafe8b4eb26ca4c8d
hamin <ggamini7@gmail.com>
2023-03-06 13:55:06 +0900

    13:54

--- src/main/java/com/kt/myrestapi/lectures/LectureController.java
+++ src/main/java/com/kt/myrestapi/lectures/LectureController.java
@@ -1,5 +1,6 @@
 package com.kt.myrestapi.lectures;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
@@ -13,13 +14,16 @@ import java.net.URI;
 
 @Controller
 @RequestMapping(value = "/api/lectures", produces = MediaTypes.HAL_JSON_VALUE)
+@RequiredArgsConstructor
 public class LectureController {
+    private final LectureRepository lectureRepository;
+
     @PostMapping
     public ResponseEntity createLecture(@RequestBody Lecture lecture) {
-        lecture.setId(10);
+        Lecture savedLecture = lectureRepository.save(lecture);
         WebMvcLinkBuilder selfLinkBuilder = WebMvcLinkBuilder.linkTo(LectureController.class)
                 .slash(lecture.getId());
         URI createUri = selfLinkBuilder.toUri();
-        return ResponseEntity.created(createUri).body(lecture);
+        return ResponseEntity.created(createUri).body(savedLecture);
     }
 }
