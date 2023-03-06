79f78d8033794021c307bb5ea10001bb81875ab9
hamin <ggamini7@gmail.com>
2023-03-06 10:34:05 +0900

    first commit

---
+++ src/main/java/com/kt/myrestapi/lectures/Lecture.java
@@ -0,0 +1,16 @@
+package com.kt.myrestapi.lectures;
+
+import java.time.LocalDateTime;
+
+public class Lecture {
+    private Integer id;
+    private String name;
+    private String description;
+    private LocalDateTime beginEnrollmentDateTime; private LocalDateTime closeEnrollmentDateTime; private LocalDateTime beginLectureDateTime; private LocalDateTime endLectureDateTime; private String location;
+    private int basePrice;
+    private int maxPrice;
+    private int limitOfEnrollment;
+    private boolean offline;
+    private boolean free;
+    private LectureStatus lectureStatus = LectureStatus.DRAFT;
+}
