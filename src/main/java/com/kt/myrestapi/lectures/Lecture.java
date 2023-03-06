4adebd9a36de6a995d55b9e76c79a7419338e693
hamin <ggamini7@gmail.com>
2023-03-06 13:46:50 +0900

    13:46

--- src/main/java/com/kt/myrestapi/lectures/Lecture.java
+++ src/main/java/com/kt/myrestapi/lectures/Lecture.java
@@ -1,23 +1,39 @@
 package com.kt.myrestapi.lectures;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
-@Builder @AllArgsConstructor
+@Entity
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
-@EqualsAndHashCode(of="id")
+@EqualsAndHashCode(of = "id")
 public class Lecture {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @Column(nullable = false)
     private String name;
     private String description;
-    private LocalDateTime beginEnrollmentDateTime; private LocalDateTime closeEnrollmentDateTime; private LocalDateTime beginLectureDateTime; private LocalDateTime endLectureDateTime; private String location;
+    private boolean offline;
+    private boolean free;
+    @Enumerated(EnumType.STRING)
+    private LectureStatus lectureStatus = LectureStatus.DRAFT;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
+    private LocalDateTime beginEnrollmentDateTime;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
+    private LocalDateTime closeEnrollmentDateTime;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
+    private LocalDateTime beginLectureDateTime;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
+    private LocalDateTime endLectureDateTime;
+    private String location;
     private int basePrice;
     private int maxPrice;
     private int limitOfEnrollment;
-    private boolean offline;
-    private boolean free;
-    private LectureStatus lectureStatus = LectureStatus.DRAFT;
 }
