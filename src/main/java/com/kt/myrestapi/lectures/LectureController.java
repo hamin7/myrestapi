910844d2ef43e18ddd92e3f750989fe792ac39fe
mysoyul <vega2k_@naver.com>
2023-03-07 11:27:18 +0900

    findAll add

--- src/main/java/com/kt/myrestapi/lectures/LectureController.java
+++ src/main/java/com/kt/myrestapi/lectures/LectureController.java
@@ -5,11 +5,13 @@ import com.kt.myrestapi.lectures.dto.LectureReqDto;
 import com.kt.myrestapi.lectures.dto.LectureResDto;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Pageable;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,6 +29,10 @@ public class LectureController {
     private final ModelMapper modelMapper;
     private final LectureValidator lectureValidator;
 
+    @GetMapping
+    public ResponseEntity queryLectures(Pageable pageable) {
+        return ResponseEntity.ok(this.lectureRepository.findAll(pageable));
+    }
 
     @PostMapping
     public ResponseEntity createLecture(@RequestBody @Valid LectureReqDto lectureReqDto,
