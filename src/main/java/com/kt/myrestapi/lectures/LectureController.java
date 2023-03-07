efd0985b47f61f6f4c0b318b1ac2f74efc596624
mysoyul <vega2k_@naver.com>
2023-03-07 13:28:07 +0900

    findAll update

--- src/main/java/com/kt/myrestapi/lectures/LectureController.java
+++ src/main/java/com/kt/myrestapi/lectures/LectureController.java
@@ -40,9 +40,13 @@ public class LectureController {
         //Page<Lecture> => Page<LectureResDto>
         Page<LectureResDto> lectureResDtoPage =
                 lecturePage.map(lecture -> modelMapper.map(lecture, LectureResDto.class));
+        //1단계 - first,prev,next,last 링크
+        //PagedModel<org.springframework.hateoas.EntityModel<T>> toModel(Page<T> entity)
         //Page<LectureResDto> => PagedModel<EntityModel<LectureResDto>>
-        PagedModel<EntityModel<LectureResDto>> pagedResources =
-                assembler.toModel(lectureResDtoPage);
+//        PagedModel<EntityModel<LectureResDto>> pagedResources =
+//                assembler.toModel(lectureResDtoPage);
+        //2단계 - first,prev,next,last 링크 + SelfLink 도 포함
+
         return ResponseEntity.ok(pagedResources);
     }
 
