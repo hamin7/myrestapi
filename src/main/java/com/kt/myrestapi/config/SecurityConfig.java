95f8317e0aa0ab5b6a9d79949030fcdf3e2b7f2a
mysoyul <vega2k_@naver.com>
2023-03-07 17:14:28 +0900

    filter add

--- src/main/java/com/kt/myrestapi/config/SecurityConfig.java
+++ src/main/java/com/kt/myrestapi/config/SecurityConfig.java
@@ -1,6 +1,10 @@
 package com.kt.myrestapi.config;
 
+import com.kt.myrestapi.accounts.AccountService;
+import com.kt.myrestapi.filter.CustomAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -9,15 +13,35 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.anonymous()
-                .and()
-                .formLogin()
-                .and()
-                .authorizeRequests()
-                .mvcMatchers(HttpMethod.GET, "/api/**").permitAll()
-                .anyRequest()
-                .authenticated();
+    @Autowired
+    AccountService accountService;
+    Environment env;
+
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.anonymous()
+//                .and()
+//                .formLogin()
+//                .and()
+//                .authorizeRequests()
+//                .mvcMatchers(HttpMethod.GET, "/api/**").permitAll()
+//                .anyRequest()
+//                .authenticated();
+//    }
+@Override
+protected void configure(HttpSecurity http) throws Exception {
+    http.csrf().disable();
+    http.authorizeRequests()
+            .mvcMatchers(HttpMethod.GET, "/api/**").permitAll()
+            .antMatchers("/*/login", "/*/signup").permitAll()
+            .and()
+            .addFilter(getAuthenticationFilter());
+    http.headers().frameOptions().disable();
+}
+    private CustomAuthenticationFilter getAuthenticationFilter() throws Exception {
+        CustomAuthenticationFilter authenticationFilter =
+                new CustomAuthenticationFilter(authenticationManager(), accountService, env);
+        return authenticationFilter;
     }
+
 }
